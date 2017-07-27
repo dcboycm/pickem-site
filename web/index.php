187ddb6033fa8037df638b77d090e2ca565d1f33
Curtis Miller <curtis.miller@infusionsoft.com>
2017-07-27 15:00:57 -0700

    sizing the header for the form

--- web/index.php
+++ web/index.php
@@ -11,7 +11,9 @@
     <div class="form-group" id="login">
       <form method="post" class="form-horizontal">
         <div class="form-group">
-          <h2>Login <small>enter your credentials</small></h2>
+          <div class="col-sm-10">
+            <h2>Login <small>enter your credentials</small></h2>
+          </div>
           <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
           <div class="col-sm-10">
             <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
