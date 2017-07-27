94718b7ace417e4252b792542d3c4f688d3ba74e
Curtis Miller <curtis.miller@infusionsoft.com>
2017-07-27 16:29:37 -0700

    trying to get login working

---
+++ web/home-page.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Pickem Site - Home</title>
+  </head>
+  <body>
+
+  </body>
+</html>
--- web/index.php
+++ web/index.php
@@ -1,3 +1,15 @@
+<?PHP
+require_once("./include/membersite_config.php");
+
+if(isset($_POST['submitted']))
+{
+   if($fgmembersite->Login())
+   {
+        $fgmembersite->RedirectToURL("home-page.php");
+   }
+}
+
+?>
 <!DOCTYPE html>
 <html>
   <head>
