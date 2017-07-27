74c2ab5711f7ccc5cfded1224a1c49306b58cee2
Curtis Miller <curtis.miller@infusionsoft.com>
2017-07-27 14:58:29 -0700

    included the header for the form

--- web/index.php
+++ web/index.php
@@ -10,8 +10,8 @@
   <body>
     <div class="form-group" id="login">
       <form method="post" class="form-horizontal">
-        <h2>Login <small>enter your credentials</small></h2>
         <div class="form-group">
+          <h2>Login <small>enter your credentials</small></h2>
           <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
           <div class="col-sm-10">
             <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
