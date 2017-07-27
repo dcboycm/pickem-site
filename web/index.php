85c36390ddaac6f27c6f5b6847ef57de90727ec2
Curtis Miller <curtis.miller@infusionsoft.com>
2017-07-27 16:38:19 -0700

    checking something

--- web/index.php
+++ web/index.php
@@ -1,15 +1,3 @@
-<?PHP
-require_once("./include/membersite_config.php");
-
-if(isset($_POST['submitted']))
-{
-   if($fgmembersite->Login())
-   {
-        $fgmembersite->RedirectToURL("home-page.php");
-   }
-}
-
-?>
 <!DOCTYPE html>
 <html>
   <head>
