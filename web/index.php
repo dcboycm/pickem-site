dfa6fa4021e573af160eb51c91f8f8c3641dfa0d
Curtis Miller <curtis.miller@infusionsoft.com>
2017-07-28 08:35:06 -0700

    adding proper SQL query

--- web/index.php
+++ web/index.php
@@ -8,7 +8,7 @@
       $myusername = mysqli_real_escape_string($db,$_POST['email']);
       $mypassword = mysqli_real_escape_string($db,$_POST['password']);
 
-      $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+      $sql = "SELECT id FROM user WHERE email = '$myusername' and password = '$mypassword'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       // $active = $row['active'];
