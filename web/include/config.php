8c9c7c08e6e16d7a3251ed3e8d91ed7022cd57c4
Curtis Miller <curtis.miller@infusionsoft.com>
2017-07-29 15:18:25 -0700

    rolling stuff back

--- web/include/config.php
+++ web/include/config.php
@@ -1,3 +1,3 @@
 <?php
-
+  $conn = pg_connect("host=ec2-23-21-96-159.compute-1.amazonaws.com port=5432 dbname=dacj0ham29q2mi user=odlgwfrmxtsfrr password=6f3a930f7788aaa68211ef550e8137a421c55eba78ccacb15841337db2893afc");
 ?>
--- web/index.php
+++ web/index.php
@@ -2,17 +2,11 @@
 include("./include/config.php");
 session_start();
 if($_SERVER["REQUEST_METHOD"] == "POST") {
-   // username and password sent from form
-   $myusername = mysqli_real_escape_string($db,$_POST['email']);
-   $mypassword = mysqli_real_escape_string($db,$_POST['password']);
-   $sql = "SELECT id FROM user WHERE email = '$myusername' and password = '$mypassword'";
-   $result = mysqli_query($db,$sql);
-   $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-   // $active = $row['active'];
-   $count = mysqli_num_rows($result);
+
+  $result = pg_query($conn, "SELECT count(*) FROM users WHERE email = '$myusername' and password = '$mypassword'");
    // If result matched $myusername and $mypassword, table row must be 1 row
-   if($count == 1) {
-      $_SESSION['login_user'] = $myusername;
+   if($result == 1) {
+      $_SESSION['login_user'] = pg_query($conn, "SELECT first_name FROM users WHERE email = '$myusername' and password = '$mypassword'");
       header("location: home-page.php");
    }else {
       $error = "Your Login Name or Password is invalid";
