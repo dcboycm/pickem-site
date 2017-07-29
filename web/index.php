51318e4cedb2205a922648df3dab1762127d7067
Curtis Miller <curtis.miller@infusionsoft.com>
2017-07-29 16:24:56 -0700

    I think I got it this time

--- web/index.php
+++ web/index.php
@@ -13,7 +13,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
   // get the number of rows returned
   $rows = pg_num_rows($result);
 
-  error_log("*********** Rows: $rows returned. ***************");
+  file_put_contents("php://stderr", "*********** Rows: $rows returned. ***************".PHP_EOL);
 
   // If result matched $myusername and $mypassword, table row must be 1 row
   if($rows == 1) {
