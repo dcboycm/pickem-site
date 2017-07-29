4428eb7b8daa5a33b514bd85f7b9707d245b9e24
Curtis Miller <curtis.miller@infusionsoft.com>
2017-07-29 14:58:49 -0700

    rolling stuff back

--- web/include/config.php
+++ web/include/config.php
@@ -1,20 +1,3 @@
 <?php
-$dbopts = parse_url(getenv('DATABASE_URL'));
-try {
-  $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
-                 array(
-                  'pdo.server' => array(
-                     'driver'   => 'pgsql',
-                     'user' => $dbopts["user"],
-                     'password' => $dbopts["pass"],
-                     'host' => $dbopts["host"],
-                     'port' => $dbopts["port"],
-                     'dbname' => ltrim($dbopts["path"],'/')
-                     )
-                 )
-               );
-  } catch ( PDOException $e ) {
-        echo 'ERROR!';
-        print_r( $e );
-  }
+
 ?>
