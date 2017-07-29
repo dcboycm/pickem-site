77126ec43ed47cc861f09cc3efff57909c1dbdae
Curtis Miller <curtis.miller@infusionsoft.com>
2017-07-29 14:57:23 -0700

    checking if the db connection is working

--- web/include/config.php
+++ web/include/config.php
@@ -1,15 +1,20 @@
 <?php
 $dbopts = parse_url(getenv('DATABASE_URL'));
-$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
-               array(
-                'pdo.server' => array(
-                   'driver'   => 'pgsql',
-                   'user' => $dbopts["user"],
-                   'password' => $dbopts["pass"],
-                   'host' => $dbopts["host"],
-                   'port' => $dbopts["port"],
-                   'dbname' => ltrim($dbopts["path"],'/')
-                   )
-               )
-);
+try {
+  $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+                 array(
+                  'pdo.server' => array(
+                     'driver'   => 'pgsql',
+                     'user' => $dbopts["user"],
+                     'password' => $dbopts["pass"],
+                     'host' => $dbopts["host"],
+                     'port' => $dbopts["port"],
+                     'dbname' => ltrim($dbopts["path"],'/')
+                     )
+                 )
+               );
+  } catch ( PDOException $e ) {
+        echo 'ERROR!';
+        print_r( $e );
+  }
 ?>
