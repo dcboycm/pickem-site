d5837f973d1d98dfbebf4bd1f3bb255584f00005
Curtis Miller <curtis.miller@infusionsoft.com>
2017-07-29 14:26:36 -0700

    fixing postgres connection

--- web/include/config.php
+++ web/include/config.php
@@ -1,7 +1,7 @@
 <?php
-   define('DB_SERVER', 'ec2-23-21-96-159.compute-1.amazonaws.com');
-   define('DB_USERNAME', 'odlgwfrmxtsfrr');
-   define('DB_PASSWORD', '6f3a930f7788aaa68211ef550e8137a421c55eba78ccacb15841337db2893afc');
-   define('DB_DATABASE', 'dacj0ham29q2mi');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+  define('DB_HOST', 'ec2-23-21-96-159.compute-1.amazonaws.com');
+  define('DB_NAME', 'dacj0ham29q2mi');
+  define('DB_USER', 'odlgwfrmxtsfrr');
+  define('DB_PASS', '6f3a930f7788aaa68211ef550e8137a421c55eba78ccacb15841337db2893afc');
+  $db_connection = pg_connect("host=localhost dbname=DBNAME user=USERNAME password=PASSWORD");
 ?>
