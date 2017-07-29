a33183b58df8a7ae13c268f672e90f7cd510e476
Curtis Miller <curtis.miller@infusionsoft.com>
2017-07-29 10:39:06 -0700

    adding style to error

--- web/index.php
+++ web/index.php
@@ -40,13 +40,6 @@
       	<div class="row">
       	    <div class="col-xs-12">
           	    <div class="form-wrap">
-                  <!-- <?php
-                    if ($error != "") {
-                      <div class="alert alert-danger" role="alert">
-                        echo $error;
-                      </div>
-                    }
-                  ?> -->
                   <h1>Log in with your email account</h1>
                       <form role="form" action="/" method="post" id="login-form" autocomplete="off">
                           <div class="form-group">
