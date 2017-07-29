bcc93d5b4574e4eb4d2db8a573afcc9541dcc447
Curtis Miller <curtis.miller@infusionsoft.com>
2017-07-29 13:57:11 -0700

    wiring up the database

--- web/include/config.php
+++ web/include/config.php
@@ -1,7 +1,7 @@
 <?php
-   define('DB_SERVER', 'localhost:3306');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'root');
-   define('DB_DATABASE', 'pickem');
+   define('DB_SERVER', 'ec2-23-21-96-159.compute-1.amazonaws.com');
+   define('DB_USERNAME', 'odlgwfrmxtsfrr');
+   define('DB_PASSWORD', '6f3a930f7788aaa68211ef550e8137a421c55eba78ccacb15841337db2893afc');
+   define('DB_DATABASE', 'dacj0ham29q2mi');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
