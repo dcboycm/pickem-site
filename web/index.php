e33905f2c00c1a48a56711347bdf5377be8afa1d
Curtis Miller <curtis.miller@infusionsoft.com>
2017-07-29 14:53:58 -0700

    checking if the db connection is working

--- web/index.php
+++ web/index.php
@@ -1,5 +1,23 @@
 <?php
-
+include("./include/config.php");
+session_start();
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+   // username and password sent from form
+   $myusername = mysqli_real_escape_string($db,$_POST['email']);
+   $mypassword = mysqli_real_escape_string($db,$_POST['password']);
+   $sql = "SELECT id FROM user WHERE email = '$myusername' and password = '$mypassword'";
+   $result = mysqli_query($db,$sql);
+   $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+   // $active = $row['active'];
+   $count = mysqli_num_rows($result);
+   // If result matched $myusername and $mypassword, table row must be 1 row
+   if($count == 1) {
+      $_SESSION['login_user'] = $myusername;
+      header("location: home-page.php");
+   }else {
+      $error = "Your Login Name or Password is invalid";
+   }
+}
 ?>
 <!DOCTYPE html>
 <html>
