01bce3e9b709b81ea2b9be80c53ad2efb418cb7e
Curtis Miller <curtis.miller@infusionsoft.com>
2017-07-29 14:52:35 -0700

    test

--- web/include/config.php
+++ web/include/config.php
@@ -13,9 +13,3 @@ $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider
                )
 );
 ?>
-
-<!-- define('DB_HOST', 'ec2-23-21-96-159.compute-1.amazonaws.com');
-define('DB_NAME', 'dacj0ham29q2mi');
-define('DB_USER', 'odlgwfrmxtsfrr');
-define('DB_PASS', '6f3a930f7788aaa68211ef550e8137a421c55eba78ccacb15841337db2893afc');
-$db_connection = pg_connect("host=localhost dbname=DBNAME user=USERNAME password=PASSWORD"); -->
