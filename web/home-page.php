bea3ef82eeac9e503bfa2edf78baa02e938e6484
Curtis Miller <curtis.miller@infusionsoft.com>
2017-07-30 16:32:04 -0700

    modifying navbar

--- web/home-page.php
+++ web/home-page.php
@@ -33,7 +33,7 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
+            <li class="active"><a href="#"><i class="glyphicon glyphicon-home">Home</i></a></li>
             <!-- <li class="active"><a href="#"><i class="glyphicon glyphicon-home"</i>Home<span class="sr-only">(current)</span></a></li> -->
             <!-- <li><a href="#"><i class="glyphicon glyphicon-file"</i>Make Picks</a></li>
             <li><a href="#"><i class="glyphicon glyphicon-th"</i>Everyone's Picks</a></li>
