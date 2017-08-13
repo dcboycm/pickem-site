4ddf14822b33c7c52619e12c5ade8551249ee53e
Curtis Miller <curtis.miller@infusionsoft.com>
2017-08-12 22:46:44 -0700

    getting weekly match data

--- web/make-picks.php
+++ web/make-picks.php
@@ -125,22 +125,24 @@
       foreach ($rows as $row) {
         echo "<tbody>";
           echo "<tr>";
-            echo "<td><input class='single-checkbox'type='checkbox' name='picked' value='Selected'><br></td>";
             // Need an if check for who is the FAVORITE, this will determine who goes first in the list
             $result = pg_query($conn, "select fav_name from team where id = {$row['team_home']};");
             $home_fav = pg_fetch_row($result);
             $result = pg_query($conn, "select fav_name from team where id = {$row['team_away']};");
             $away_fav = pg_fetch_row($result);
             if ($row[team_home] == $row[team_fav]) {
+              echo "<td><input class='single-checkbox'type='checkbox' name='picked' value='{$row[team_home]}'><br></td>";
               echo "<td>{$home_fav[0]}</td>";
               echo "<td>{$row['spread']}</td>";
               echo "<td>{$away_fav[0]}</td>";
+              echo "<td><input class='single-checkbox'type='checkbox' name='picked' value='{$row[team_away]}'><br></td>";
             } else {
+              echo "<td><input class='single-checkbox'type='checkbox' name='picked' value='{$row[team_away]}'><br></td>";
               echo "<td>{$away_fav[0]}</td>";
               echo "<td>{$row['spread']}</td>";
               echo "<td>{$home_fav[0]}</td>";
+              echo "<td><input class='single-checkbox'type='checkbox' name='picked' value='{$row[team_home]}'><br></td>";
             }
-            echo "<td><input class='single-checkbox'type='checkbox' name='picked' value='Selected'><br></td>";
           echo "</tr>";
         echo "</tbody>";
         $i++;
