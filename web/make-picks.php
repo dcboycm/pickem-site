f5ce0c05b82721f53d9b697390a77409134b8e55
Curtis Miller <curtis.miller@infusionsoft.com>
2017-08-14 08:09:35 -0700

    getting weekly match data

--- web/make-picks.php
+++ web/make-picks.php
@@ -41,6 +41,8 @@
         if(selected == 5 && tiebreaker != null){
           return true;
         } else {
+          alert("You must select 5 teams and/or set the tiebreaker.");
+          document.getElementById("tiebreaker").style.borderColor = "#E34234";
           return false;
         }
       }
@@ -81,7 +83,7 @@
   <div class="center" id="make-picks">
 		<h1>Football Pool 2017-2018</h1>
     <h2>Week 1 - September 7th - September 11th</h2>
-    <form action="submit-picks.php" method="post" onsubmit="submitSheet()">
+    <form action="submit-picks.php" method="post" onsubmit="return submitSheet();">
       <table class="table center">
         <thead>
           <h2>Thursday</h2>
