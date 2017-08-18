03e2ac732cc472d233b030a642980d9c073a775a
Curtis Miller <curtis.miller@infusionsoft.com>
2017-08-17 21:28:24 -0700

    working on submitting sheets

--- web/submit-picks.php
+++ web/submit-picks.php
@@ -12,8 +12,7 @@
   $result = pg_query($conn, "SELECT id FROM users WHERE email = $myusername;");
   $userid = pg_fetch_row($result);
   // $insertResult = pg_query($conn, "INSERT INTO submitted_sheets(user_id, pick_1, pick_2, pick_3, pick_4, pick_5, tiebreaker, paid) VALUES ($userid, {$selectedTeams[0]}, {$selectedTeams[1]}, {$selectedTeams[2]}, {$selectedTeams[3]}, {$selectedTeams[4]}, $tiebreakerPoints, false);");
-  $insertResult = pg_query($conn, "INSERT INTO submitted_sheets(user_id, pick_1, pick_2, pick_3, pick_4, pick_5, tiebreaker, paid) VALUES (1, 1, 2, 3, 4, 5, 65, false);");
-  $sheetId = pg_fetch_row($insertResult);
+  $insertResult = pg_query($conn, "INSERT INTO submitted_sheets(user_id, pick_1, pick_2, pick_3, pick_4, pick_5, tiebreaker, paid) VALUES ($userid, 1, 2, 3, 4, 5, 65, false);");
 ?>
 
 <!DOCTYPE html>
