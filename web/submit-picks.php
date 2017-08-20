fd1dfaaf388ddfbd3545f47e3275c66e8577d17c
Curtis Miller <curtischristophermiller@gmail.com>
2017-08-20 16:08:07 -0700

    getting the insert working

--- web/submit-picks.php
+++ web/submit-picks.php
@@ -11,11 +11,11 @@
   $id = pg_fetch_row($result);
 
   $insertedPick = pg_query($conn, "INSERT INTO test_matches (user_id, pick_1, pick_2, pick_3, pick_4, pick_5, tiebreaker, paid) VALUES ('$id', '$selectedTeams[0]', '$selectedTeams[1]', '$selectedTeams[2]', '$selectedTeams[3]', '$selectedTeams[4]', '$tiebreakerPoints', false);")
-  if (!$insertedPick) {
-      $errormessage = pg_last_error();
-      echo "Error with insert: " . $errormessage;
-      exit();
-  }
+  // if (!$insertedPick) {
+  //     $errormessage = pg_last_error();
+  //     echo "Error with insert: " . $errormessage;
+  //     exit();
+  // }
 ?>
 
 <!DOCTYPE html>
