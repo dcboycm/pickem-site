450adc7d41074e2d4d41f5146c993c2609e61af9
Curtis Miller <curtis.miller@infusionsoft.com>
2017-08-19 21:42:45 -0700

    working on submitting sheets

--- web/submit-picks.php
+++ web/submit-picks.php
@@ -12,7 +12,7 @@
   $result = pg_query($conn, "SELECT id FROM users WHERE email = $myusername;");
   $id = pg_fetch_row($result);
   $userId = intval($id);
-  pg_query($conn, "INSERT INTO public.test_matches(user_id, pick_1, pick_2, pick_3, pick_4, pick_5, tiebreaker, paid) VALUES (1, 1, 1, 1, 1, 1, 1, false);") or die("Couldn't insert into database.");
+  pg_query($conn, "INSERT INTO public.test_matches(user_id, pick_1, pick_2, pick_3, pick_4, pick_5, tiebreaker, paid) VALUES ($id, 1, 1, 1, 1, 1, 1, false);") or die("Couldn't insert into database.");
 ?>
 
 <!DOCTYPE html>
