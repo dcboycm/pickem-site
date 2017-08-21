06699cd88c0fb5caed26a0c5c6353a4ff6b3037a
Curtis Miller <curtis.miller@infusionsoft.com>
2017-08-20 17:58:52 -0700

    working on submitting sheets

--- web/submit-picks.php
+++ web/submit-picks.php
@@ -7,10 +7,10 @@
   $selectedTeams = $_POST['selectedTeamId'];
   $tiebreakerPoints = $_POST['tiebreaker'];
 
-  $result = pg_query($conn, "SELECT id FROM users WHERE email = $myusername;");
-  $id = pg_fetch_row($result);
+  $result = pg_query($conn, "SELECT id FROM users WHERE email = '$myusername';");
+  $userId = pg_fetch_row($result);
 
-  $insertedPick = pg_query($conn, "INSERT INTO test_matches (user_id, pick_1, pick_2, pick_3, pick_4, pick_5, tiebreaker, paid) VALUES ('$id', '$selectedTeams[0]', '$selectedTeams[1]', '$selectedTeams[2]', '$selectedTeams[3]', '$selectedTeams[4]', '$tiebreakerPoints', false);")
+  $insertedPick = pg_query($conn, "INSERT INTO test_matches (user_id, pick_1, pick_2, pick_3, pick_4, pick_5, tiebreaker, paid) VALUES ($userId, '$selectedTeams[0]', '$selectedTeams[1]', '$selectedTeams[2]', '$selectedTeams[3]', '$selectedTeams[4]', '$tiebreakerPoints', false);")
   // if (!$insertedPick) {
   //     $errormessage = pg_last_error();
   //     echo "Error with insert: " . $errormessage;
