f2841a28bdf88d2b5e3d5ed87f7c1a45023fa95d
Curtis Miller <curtis.miller@infusionsoft.com>
2017-08-26 07:44:13 -0700

    modifying the make picks table to show who is the home team.

--- web/component/table_data.php
+++ web/component/table_data.php
@@ -14,7 +14,7 @@ echo "<tbody>";
       $away_fav = pg_fetch_row($result);
       if ($row[team_home] == $row[team_fav]) {
         echo "<td><input class='single-checkbox' type='checkbox' name='selectedTeamId[]' value='{$row[team_home]}'><br></td>";
-        echo "<td>{$home_fav[0]}</td>";
+        echo "<td>@{$home_fav[0]}</td>";
         echo "<td>{$row['spread']}</td>";
         echo "<td>{$away_fav[0]}</td>";
         echo "<td><input class='single-checkbox' type='checkbox' name='selectedTeamId[]' value='{$row[team_away]}'><br></td>";
@@ -22,7 +22,7 @@ echo "<tbody>";
         echo "<td><input class='single-checkbox' type='checkbox' name='selectedTeamId[]' value='{$row[team_away]}'><br></td>";
         echo "<td>{$away_fav[0]}</td>";
         echo "<td>{$row['spread']}</td>";
-        echo "<td>{$home_fav[0]}</td>";
+        echo "<td>@{$home_fav[0]}</td>";
         echo "<td><input class='single-checkbox' type='checkbox' name='selectedTeamId[]' value='{$row[team_home]}'><br></td>";
       }
       echo "</tr>";
