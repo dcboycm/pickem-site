be200f2350869971e2082003cdd981d765e3b6c1
Curtis Miller <curtis.miller@infusionsoft.com>
2017-08-30 20:49:18 -0700

    adding images and code to render images in table

--- web/component/table_data.php
+++ web/component/table_data.php
@@ -15,15 +15,15 @@ echo "<tbody>";
       $away_fav = pg_fetch_row($result);
       if ($row[team_home] == $row[team_fav]) {
         echo "<td><input class='single-checkbox' type='checkbox' name='selectedTeamId[]' value='{$row[team_home]}'><br></td>";
-        echo "<td>@{$home_fav[0]}</td>";
+        echo "<td><img src='./images/team_icons/{$home_fav[0]}.png'></img>@{$home_fav[0]}</td>";
         echo "<td>{$row['spread']}</td>";
-        echo "<td>{$away_fav[0]}</td>";
+        echo "<td><img src='./images/team_icons/{$away_fav[0]}.png'></img>@{$away_fav[0]}</td>";
         echo "<td><input class='single-checkbox' type='checkbox' name='selectedTeamId[]' value='{$row[team_away]}'><br></td>";
       } else {
         echo "<td><input class='single-checkbox' type='checkbox' name='selectedTeamId[]' value='{$row[team_away]}'><br></td>";
-        echo "<td>{$away_fav[0]}</td>";
+        echo "<td><img src='./images/team_icons/{$away_fav[0]}.png'></img>@{$away_fav[0]}</td>";
         echo "<td>{$row['spread']}</td>";
-        echo "<td>@{$home_fav[0]}</td>";
+        echo "<td><img src='./images/team_icons/{$home_fav[0]}.png'></img>@{$home_fav[0]}</td>";
         echo "<td><input class='single-checkbox' type='checkbox' name='selectedTeamId[]' value='{$row[team_home]}'><br></td>";
       }
       echo "</tr>";
