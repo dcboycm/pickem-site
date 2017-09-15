8c8527aefa6f9a95d9103fc18355923a4a36e517
Curtis Miller <curtis.miller@infusionsoft.com>
2017-09-15 14:36:32 -0700

    working on my picks tab

--- web/test-page.php
+++ web/test-page.php
@@ -45,46 +45,21 @@
         <td>Maria Anders</td>
         <td>Germany</td>
       </tr>
-      <tr>
-        <td>Centro comercial Moctezuma</td>
-        <td>Francisco Chang</td>
-        <td>Mexico</td>
-      </tr>
-      <tr>
-        <td>Ernst Handel</td>
-        <td>Roland Mendel</td>
-        <td>Austria</td>
-      </tr>
-      <tr>
-        <td>Island Trading</td>
-        <td>Helen Bennett</td>
-        <td>UK</td>
-      </tr>
-      <tr>
-        <td>Laughing Bacchus Winecellars</td>
-        <td>Yoshi Tannamuri</td>
-        <td>Canada</td>
-      </tr>
-      <tr>
-        <td>Magazzini Alimentari Riuniti</td>
-        <td>Giovanni Rovelli</td>
-        <td>Italy</td>
-      </tr>
     </table>
 
     <table class="table">
       <tr>
         <th>Username</th>
-        <td>Pick 1</td>
-        <td>Pick 2</td>
-        <td>Pick 3</td>
-        <td>Pick 4</td>
-        <td>Pick 5</td>
-        <td>Tiebreaker</td>
-        <td>Paid</td>
+        <th>Pick 1</th>
+        <th>Pick 2</th>
+        <th>Pick 3</th>
+        <th>Pick 4</th>
+        <th>Pick 5</th>
+        <th>Tiebreaker</th>
+        <th>Paid</th>
       </tr>
       <tr>
-        <td><?php echo "$myusername"; ?></td>
+        <tr><?php echo "$myusername"; ?></tr>
         <tr>WAS</tr>
         <tr>NE</tr>
         <tr>KC</tr>
