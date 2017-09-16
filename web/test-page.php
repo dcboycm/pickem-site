10128ee700c14566eb8b6681ac634834a79d918c
Curtis Miller <curtis.miller@infusionsoft.com>
2017-09-15 20:32:32 -0700

    working on my picks tab

--- web/test-page.php
+++ web/test-page.php
@@ -52,7 +52,7 @@
             foreach ($picks as $pick) {
               $nicknameResult = pg_query($conn, "SELECT nickname FROM users WHERE email = $pick[user_id];")
               $nicknameFetched = pg_fetch_row($nicknameResult);
-              echo "<td>$nicknameFetched[0]</td>";
+              echo "<td>{$nicknameFetched[0]}</td>";
               $favNameResult = pg_query($conn, "SELECT fav_name FROM team WHERE id = $pick[pick_1];");
               $team_fav_name = pg_fetch_row($favNameResult);
               echo "<td>{$team_fav_name[0]}</td>";
