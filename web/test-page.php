026b8a81fa01328fc132720e0ecff117d2314e28
Curtis Miller <curtis.miller@infusionsoft.com>
2017-09-18 18:59:24 -0700

    working on my picks tab

--- web/test-page.php
+++ web/test-page.php
@@ -52,7 +52,7 @@
           echo "<h2>Total Pot Size - $$totalPot.</h2>";
         }
         foreach ($winners as $winner) {
-          print $winner;
+          print_r($winner);
         }
       ?>
       <table class="table center">
