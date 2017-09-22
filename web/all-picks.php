61ce0c3eec7d01357ee5516511a0323bf0480dff
Curtis Miller <curtis.miller@infusionsoft.com>
2017-09-22 12:26:05 -0700

    updating the rollover amount

--- web/all-picks.php
+++ web/all-picks.php
@@ -23,7 +23,7 @@
    $result = pg_query($conn, "SELECT * FROM test_matches WHERE week = $week_number and paid = true;");
    $pickCount = pg_num_rows($result);
    $sheet = 5;
-   $rollover = 133;
+   $rollover = 261;
    $totalPot = (($sheet * $pickCount) + $rollover) - 5;
 ?>
 <!DOCTYPE html>
