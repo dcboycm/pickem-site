d4f84d1aec21b53081b06af4333552262f58ca60
Curtis Miller <curtis.miller@infusionsoft.com>
2017-09-23 21:27:01 -0700

    extending picks to start of first Sunday game.

--- web/make-picks.php
+++ web/make-picks.php
@@ -159,7 +159,7 @@
       </table>
 
         <div class="tiebreaker">
-          <h2>Tie-Breaker Points: <?php echo "DEN/LAC" ?></h2>
+          <h2>Tie-Breaker Points: <?php echo "DAL/ARI" ?></h2>
         </div>
         <div style="text-align: center;" class="tiebreaker-points center">
           <input style="text-align: center;" type="text" name="tiebreaker" id="tiebreaker" placeholder="50">  pts.</input>
