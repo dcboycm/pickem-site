6bc1d27c21b3588035d697fe49c9a699087d69f7
Curtis Miller <curtischristophermiller@gmail.com>
2017-10-11 21:53:56 -0700

    week 6 order updates.

--- web/make-picks.php
+++ web/make-picks.php
@@ -109,7 +109,7 @@
           </tr>
         </thead>
       <?php
-      $result = pg_query($conn, "select * from weekly_matches where week_number = '$week_number' and match_date = '2017-10-12' order by match_date;");
+      $result = pg_query($conn, "select * from weekly_matches where week_number = '$week_number' and match_date = '2017-10-12' order by id asc;");
         if ($today == "Thursday" && $time >= "17:25:00") {
           include('./component/thursday_data.php');
         } else if ($today == "Friday" || $today == "Saturday") {
@@ -132,7 +132,7 @@
           </tr>
         </thead>
       <?php
-      $result = pg_query($conn, "select * from weekly_matches where week_number = '$week_number' and match_date = '2017-10-15' order by match_date;");
+      $result = pg_query($conn, "select * from weekly_matches where week_number = '$week_number' and match_date = '2017-10-15' order by id asc;");
         include('./component/table_data.php');
       ?>
       </table>
@@ -149,7 +149,7 @@
           </tr>
         </thead>
       <?php
-      $result = pg_query($conn, "select * from weekly_matches where week_number = '$week_number' and match_date = '2017-10-16' order by match_date;");
+      $result = pg_query($conn, "select * from weekly_matches where week_number = '$week_number' and match_date = '2017-10-16' order by id asc;");
       include('./component/table_data.php');
       ?>
       </table>
