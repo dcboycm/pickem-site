4366cf12488d7faf86a53f19f34f94e9e6348a7f
Curtis Miller <curtis.miller@cycosoft.com>
2018-10-30 08:47:48 -0700

    Week 9 updates

--- scripts/inster_matches.sql
+++ scripts/inster_matches.sql
@@ -2,7 +2,7 @@
 select * from team;
 
 -- get all games for a specific week
-select * from weekly_matches WHERE week_number = 2;
+select * from weekly_matches WHERE week_number = 9 order by id asc;
 
 -- insert a game for the week
 INSERT INTO weekly_matches (team_home, team_away, team_fav, spread, week_number, week_year, match_date)
