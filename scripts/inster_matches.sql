aff78a9ea4ef1eb1044881978931593aff1b9eac
Curtis Miller <curtischristophermiller@gmail.com>
2018-12-18 20:45:54 -0700

    week 16 updates

--- scripts/inster_matches.sql
+++ scripts/inster_matches.sql
@@ -6,34 +6,34 @@ select * from weekly_matches WHERE week_number = 12 order by id asc;
 
 -- insert a game for the week
 INSERT INTO weekly_matches (team_home, team_away, team_fav, spread, week_number, week_year, match_date)
-VALUES(31, 15, 31, 4, 14, 2018, '2018-12-06');
+VALUES(31, 32, 31, 10, 16, 2018, '2018-12-22');
 INSERT INTO weekly_matches (team_home, team_away, team_fav, spread, week_number, week_year, match_date)
-VALUES(16, 3, 16, 6.5, 14, 2018, '2018-12-09');
+VALUES(17, 3, 17, 4.5, 16, 2018, '2018-12-22');
 INSERT INTO weekly_matches (team_home, team_away, team_fav, spread, week_number, week_year, match_date)
-VALUES(13, 14, 13, 4.5, 14, 2018, '2018-12-09');
+VALUES(9, 30, 9, 7, 16, 2018, '2018-12-23');
 INSERT INTO weekly_matches (team_home, team_away, team_fav, spread, week_number, week_year, match_date)
-VALUES(8, 5, 5, 1.5, 14, 2018, '2018-12-09');
+VALUES(21, 4, 21, 12.5, 16, 2018, '2018-12-23');
 INSERT INTO weekly_matches (team_home, team_away, team_fav, spread, week_number, week_year, match_date)
-VALUES(12, 2, 12, 5.5, 14, 2018, '2018-12-09');
+VALUES(5, 2, 5, 2.5, 16, 2018, '2018-12-23');
 INSERT INTO weekly_matches (team_home, team_away, team_fav, spread, week_number, week_year, match_date)
-VALUES(30, 22, 22, 8, 14, 2018, '2018-12-09');
+VALUES(19, 15, 19, 4.5, 16, 2018, '2018-12-23');
 INSERT INTO weekly_matches (team_home, team_away, team_fav, spread, week_number, week_year, match_date)
-VALUES(4, 24, 4, 3.5, 14, 2018, '2018-12-09');
+VALUES(14, 23, 14, 9.5, 16, 2018, '2018-12-23');
 INSERT INTO weekly_matches (team_home, team_away, team_fav, spread, week_number, week_year, match_date)
-VALUES(19, 21, 21, 7.5, 14, 2018, '2018-12-09');
+VALUES(26, 13, 26, 1.5, 16, 2018, '2018-12-23');
 INSERT INTO weekly_matches (team_home, team_away, team_fav, spread, week_number, week_year, match_date)
-VALUES(6, 18, 18, 3, 14, 2018, '2018-12-09');
+VALUES(11, 20, 20, 5.5, 16, 2018, '2018-12-23');
 INSERT INTO weekly_matches (team_home, team_away, team_fav, spread, week_number, week_year, match_date)
-VALUES(32, 23, 23, 3.5, 14, 2018, '2018-12-09');
+VALUES(24, 12, 12, 3, 16, 2018, '2018-12-23');
 INSERT INTO weekly_matches (team_home, team_away, team_fav, spread, week_number, week_year, match_date)
-VALUES(28, 10, 10, 5.5, 14, 2018, '2018-12-09');
+VALUES(8, 7, 8, 7.5, 16, 2018, '2018-12-23');
 INSERT INTO weekly_matches (team_home, team_away, team_fav, spread, week_number, week_year, match_date)
-VALUES(17, 7, 17, 14, 14, 2018, '2018-12-09');
+VALUES(1, 18, 18, 14, 16, 2018, '2018-12-23');
 INSERT INTO weekly_matches (team_home, team_away, team_fav, spread, week_number, week_year, match_date)
-VALUES(1, 11, 11, 2.5, 14, 2018, '2018-12-09');
+VALUES(28, 6, 6, 4, 16, 2018, '2018-12-23');
 INSERT INTO weekly_matches (team_home, team_away, team_fav, spread, week_number, week_year, match_date)
-VALUES(9, 26, 9, 3.5, 14, 2018, '2018-12-09');
+VALUES(22, 27, 22, 5.5, 16, 2018, '2018-12-23');
 INSERT INTO weekly_matches (team_home, team_away, team_fav, spread, week_number, week_year, match_date)
-VALUES(25, 27, 27, 11, 14, 2018, '2018-12-09');
+VALUES(29, 16, 16, 2.5, 16, 2018, '2018-12-23');
 INSERT INTO weekly_matches (team_home, team_away, team_fav, spread, week_number, week_year, match_date)
-VALUES(29, 20, 29, 3.5, 14, 2018, '2018-12-10');
+VALUES(25, 10, 10, 2.5, 16, 2018, '2018-12-24');
--- scripts/query.sql
+++ scripts/query.sql
@@ -1,4 +1,4 @@
-SELECT * FROM test_matches WHERE week = 14 ORDER BY user_id ASC;
+SELECT * FROM test_matches WHERE week = 16 ORDER BY user_id ASC;
 
 select * from users;
 
@@ -9,12 +9,12 @@ UPDATE test_matches SET paid = true WHERE id = 550;
 SELECT test_matches.id, nickname, pick_1, pick_2, pick_3, pick_4, pick_5, tiebreaker, paid, week, created_on_date
 	FROM test_matches
 	INNER JOIN users ON (test_matches.user_id = users.email)
-	WHERE week = 14 ORDER BY id ASC;
+	WHERE week = 16 ORDER BY id ASC;
     
 SELECT test_matches.id, nickname, pick_1, pick_2, pick_3, pick_4, pick_5, tiebreaker, paid, week, created_on_date
 	FROM test_matches
 	INNER JOIN users ON (test_matches.user_id = users.email)
-	WHERE week = 14 AND paid = false ORDER BY id ASC;
+	WHERE week = 16 AND paid = false ORDER BY id ASC;
 	
 select user_id, pick_1, pick_2, pick_3, pick_4, pick_5, tiebreaker, paid from test_matches where week = 2 and paid = true;
 SELECT nickname FROM users WHERE email = 'curtischristophermiller@gmail.com';
@@ -23,7 +23,7 @@ select * from test_matches where user_id = 'curtischristophermiller@gmail.com' a
 UPDATE test_matches SET pick_5 = 1 WHERE id = 316;
 
 INSERT into test_matches (id, user_id, pick_1, pick_2, pick_3, pick_4, pick_5, tiebreaker, paid, week, created_on_date)
-	VALUES (518, 'david.galvan12@gmail.com', 14, 26, 8, 27, 16, 54, true, 4, '2018-09-26 14:12:54.717759+00');
+	VALUES (678, 'curtischristophermiller@gmail.com', 24, 19, 12, 29, 22, 45, true, 15, '2018-12-15 04:30:55.86527+00');
 	
 DELETE FROM test_matches WHERE id = 440;
 
